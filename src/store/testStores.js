5d1c679414c7343c6da511847d7999ff025bf034
Kuchuk Andrey <kreezag@gmail.com>
2019-02-03 23:57:35 +0300

    add items in test-articles

--- src/store/testStores.js
+++ src/store/testStores.js
@@ -65,6 +65,52 @@ const articleStore = {
           color: '#f15931'
         }
       ]
+    },
+    {
+      publisher: 'MY fruend',
+      title: 'best article',
+      description: 'two versino some long description',
+      link: 'http://habrahabr.ru/post/148050/',
+      image: 'https://www.iab.com/wp-content/uploads/2016/03/html5-advertising-platform-showcase-2-1024x1020.jpg',
+      stars: '4.5',
+      date: new Date(),
+      labels: [
+        {
+          uuid: '@JS',
+          text: 'JS',
+          shortTitle: 'JS',
+          color: '#f09'
+        },
+        {
+          uuid: '@HTML',
+          text: 'HTML',
+          shortTitle: 'HTML',
+          color: '#f15931'
+        }
+      ]
+    },
+    {
+      publisher: 'MY fruend',
+      title: 'best article',
+      description: 'two versino some long description',
+      link: 'http://habrahabr.ru/post/148050/',
+      image: 'https://www.iab.com/wp-content/uploads/2016/03/html5-advertising-platform-showcase-2-1024x1020.jpg',
+      stars: '4.5',
+      date: new Date(),
+      labels: [
+        {
+          uuid: '@JS',
+          text: 'JS',
+          shortTitle: 'JS',
+          color: '#f09'
+        },
+        {
+          uuid: '@HTML',
+          text: 'HTML',
+          shortTitle: 'HTML',
+          color: '#f15931'
+        }
+      ]
     }
   ]
 };
